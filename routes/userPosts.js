cc3b65110053d6dbfa7b0874b6bff1646eb84579
alokMIPL <alok@madhulata.com>
2026-01-29 14:59:03 +0530

    code added

---
+++ routes/userPosts.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { createPost, deletePost, fetchPosts, showPost, updatePost } from "../Controller/PostController";
+
+
+const router = Router();
+
+router.get("/", fetchPosts);
+router.get("/:id", showPost);
+router.post("/", createPost);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
+
+export default router;
