7ff0064493701c4c56b004a3a0a717a2b224bd8c
alokMIPL <alok@madhulata.com>
2026-01-29 14:35:06 +0530

    code added

--- Controller/PostController.js
+++ Controller/PostController.js
@@ -3,8 +3,12 @@ import prisma from "../DB/db.config";
 // get all posts
 export const fetchPosts = async (req, res) => {
   const posts = await prisma.post.findMany({});
-  return res.json({ status: 200, data: posts, message: "Posts Data Fetched Successfully." })
-}
+  return res.json({
+    status: 200,
+    data: posts,
+    message: "Posts Data Fetched Successfully.",
+  });
+};
 
 // create new post
 export const createPost = async (req, res) => {
@@ -13,8 +17,27 @@ export const createPost = async (req, res) => {
     data: {
       user_id: Number(user_id),
       title,
-      description
-    }
-  })
-  return res.json({ status: 200, message: "Post Created Successfully", data: newPost })
-}
+      description,
+    },
+  });
+  return res.json({
+    status: 200,
+    message: "Post Created Successfully",
+    data: newPost,
+  });
+};
+
+// get post by ID
+export const showPost = async (req, res) => {
+  const postId = req.params.id;
+  const post = await prisma.post.findFirst({
+    where: {
+      id: Number(postId),
+    },
+  });
+  return res.json({
+    status: 200,
+    data: post,
+    message: "Post Data By ID Fetched Successfully.",
+  });
+};
