83676828a9d29c313652664b22a819bb75d77e73
alokMIPL <alok@madhulata.com>
2026-01-29 16:14:59 +0530

    code added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import UserRouter from "./userRoutes.js";
+import PostRouter from "./postRoutes.js";
 
 const router = Router();
 
 router.use("/api/user", UserRouter);
+router.use("/api/post", PostRouter);
 
 export default router;
